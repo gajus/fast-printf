616bd747048a88844c258e20e4e285620ed3b65b
Gajus Kuizinas <gajus@gajus.com>
2021-01-02 18:02:59 -0600

    refactor: use named groups

--- src/sprintf.ts
+++ src/sprintf.ts
@@ -2,44 +2,48 @@ import {
   format as formatNumber,
 } from 'mathjs';
 
-export const sprintf = (subject: string, ...args: any[]): string => {
+export const sprintf = (subject: string, ...boundValues: any[]): string => {
   let index = -1;
 
-  return subject.replaceAll(/(%[%c-fosux])|(\\%)/g, (match) => {
+  return subject.replaceAll(/(?:%(?<conversion>[%c-fosux]))|(\\%)/g, (match, ...args) => {
     if (match === '\\%' || match === '%%') {
       return '%';
     }
 
+    const {
+      conversion,
+    } = args[args.length - 1];
+
     index++;
 
-    const boundValue = args[index];
+    const boundValue = boundValues[index];
 
-    if (match === '%c') {
+    if (conversion === 'c') {
       if (boundValue.length !== 1) {
         throw new Error('%c must be bound to a single character');
       }
 
       return boundValue;
-    } else if (match === '%d') {
+    } else if (conversion === 'd') {
       return boundValue;
-    } else if (match === '%e') {
+    } else if (conversion === 'e') {
       return formatNumber(
         boundValue,
         {
           notation: 'exponential',
         },
       );
-    } else if (match === '%f') {
+    } else if (conversion === 'f') {
       return boundValue;
-    } else if (match === '%i') {
+    } else if (conversion === 'i') {
       return boundValue;
-    } else if (match === '%o') {
+    } else if (conversion === 'o') {
       return (Number.parseInt(boundValue, 10) >>> 0).toString(8);
-    } else if (match === '%s') {
+    } else if (conversion === 's') {
       return boundValue;
-    } else if (match === '%u') {
+    } else if (conversion === 'u') {
       return Number.parseInt(boundValue, 10) >>> 0;
-    } else if (match === '%x') {
+    } else if (conversion === 'x') {
       return (Number.parseInt(boundValue, 10) >>> 0).toString(16);
     } else {
       throw new Error('Unknown format specifier.');
